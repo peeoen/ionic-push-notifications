eee684494734036741c813dfddde105a9672d72c
peeoen <theerawat.pon@gmail.com>
2018-10-20 20:31:14 +0700

    add existing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
+import { Firebase } from '@ionic-native/firebase';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { StatusBar } from '@ionic-native/status-bar';
+import { AlertController, Platform } from 'ionic-angular';
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, 
+    private alertCtrl: AlertController,
+    firebase: Firebase) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      firebase.getToken().then(token => console.log(token)).catch(err=> console.log(err));
+      firebase.onNotificationOpen().subscribe(data=>{
+        console.log(data);
+        // console.log(data.name)
+        let alert = this.alertCtrl.create({
+          title: 'Parameters',
+          message: JSON.stringify(data)
+        });
+        alert.present();
+      }, err=> console.log(err));
+
       statusBar.styleDefault();
       splashScreen.hide();
     });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { BrowserModule } from '@angular/platform-browser';
+import { Firebase } from '@ionic-native/firebase';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
-import { MyApp } from './app.component';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { HomePage } from '../pages/home/home';
+import { MyApp } from './app.component';
+
+const firebase = {
+  apiKey: "AIzaSyDWp-Y6kBxO_vjL3Kpuez54KvE8LAN9PBU",
+  authDomain: "work-medium.firebaseapp.com",
+  databaseURL: "https://work-medium.firebaseio.com",
+  projectId: "work-medium",
+  storageBucket: "work-medium.appspot.com",
+  messagingSenderId: "389575909358"
+ }
 
 @NgModule({
   declarations: [
@@ -14,6 +25,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebase), 
+    AngularFirestoreModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,6 +37,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Firebase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
